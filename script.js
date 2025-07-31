2cc3b09fe9db45c5fe763870b5834d0f8443a2f3
ItsCarlos415 <ascencioc115@gmail.com>
2025-07-30 18:35:00 -0700

    Begin to add the airtable into javascript

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 "use strict";
 
-let n1 = 3;
-let n2 = 5;
+// function for our list view
+async function getAllRecords() {
+  let getResultElement = document.getElementById("brews");
 
-console.log (n1+ n2);
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer patfEJjmGd4egCkYT.ad361792ce913f19954a8150fbacdf0e717d16b069ff67e53b2a820af838b479`,
+    },
+  };
+
+  await fetch(
+    `https://api.airtable.com/v0/app3ztynCnkXsjtRL/Data`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // response is an object w/ .records array
+
+      getResultElement.innerHTML = ""; // clear brews
+
+      let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let logo = data.records[i].fields["Logo"]; // here we are getting column values
+        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
+        let neighborhood = data.records[i].fields["Neighborhood"];
+
+        newHtml += `
+        
+         <div class="col-xl-4 cardImageText">
+          <div class="card list move border-dark mb-5" style="width: 20rem;">
+          <a href="breweries.html?id=${data.records[i].id}">${
+          logo
+            ? `<img class="card-img-top rounded" alt="${name}" src="${logo[0].url}">`
+            : ``
+        }
+          </a>
+          <p hidden class="card-key">${neighborhood}</p>
+          </div>
+          </div>
+        </div>
+    
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
+    });
+}
+
+ getAllRecords(); // no id given, fetch summaries
