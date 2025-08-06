f679f2d473f35a7399c6b20914ad342288b8ff56
ItsCarlos415 <ascencioc115@gmail.com>
2025-08-05 17:58:35 -0700

    Added more javascript, added info onto the cards. I have to add menu, images and hours after and then i'll work html and css

--- script.js
+++ script.js
@@ -27,10 +27,33 @@ async function getAllRecords() {
         let logo = data.records[i].fields["Logo"]; // here we are getting column values
         let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
         let neighborhood = data.records[i].fields["Neighborhood"];
+        let phone = data.records[i].fields["Phone"];
+        let location = data.records[i].fields["Location"];
+        let description = data.records[i].fields["Description"]
+        let hours = data.records[i].fields["Hours"];
+        let reviews = data.records[i].fields["Reviews"];
+        let eats = data.records[i].fields["Eats"]; 
+        let favmeal = data.records[i].fields["FavMeal"];
+        
 
         newHtml += `
         
-         <div>${name}</div>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6>${phone}</h6>
+            <h6>${location}</h6>
+            <h7> Description: ${description}</h7><br></br>
+            <p>${hours} 🕰️</p>
+            <p>Star Reviews: ${reviews} ⭐️</p>
+            <h7> ${eats}</h7>
+            <p> Favorite Meal: ${favmeal} 😁</p>
+            
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
     
         
         `;
